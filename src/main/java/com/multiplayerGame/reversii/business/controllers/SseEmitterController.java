11c2309fe68e0d9f2d67e879de327eac1395d708
ankitjana <ankit92005@gmail.com>
2020-05-14 00:28:05 -0400

    sse controller

--- src/main/java/com/multiplayerGame/reversii/business/controllers/SseEmitterController.java
+++ src/main/java/com/multiplayerGame/reversii/business/controllers/SseEmitterController.java
@@ -1,9 +1,12 @@
 package com.multiplayerGame.reversii.business.controllers;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -27,4 +30,27 @@ public class SseEmitterController {
         });
         return emitter;
     }
+
 }
+
+/*
+    public void consumeServerSentEvent() {
+        WebClient client = WebClient.create("http://localhost:8080/sse");
+        ParameterizedTypeReference<ServerSentEvent<String>> type
+                = new ParameterizedTypeReference<ServerSentEvent<String>>() {};
+
+        Flux<ServerSentEvent<String>> eventStream = client.get()
+                .uri("/stream-sse")
+                .retrieve()
+                .bodyToFlux(type);
+
+        eventStream.subscribe(
+                content -> logger.info("Time: {} - event: name[{}], id [{}], content[{}] ",
+                        LocalTime.now(), content.event(), content.id(), content.data()),
+                error -> logger.error("Error receiving SSE: {}", error),
+                () -> logger.info("Completed!!!"));
+    }
+ */
+
+
+
